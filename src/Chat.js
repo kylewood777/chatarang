1834f8df6e20dff06eaf9d325e0e0955eaa9e274
kyle wood <kylewood777@oru.edu>
2018-06-14 14:33:20 -0400

    save rooms messages

--- src/Chat.js
+++ src/Chat.js
@@ -10,18 +10,35 @@ class Chat extends Component {
     super()
 
     this.state = {
-      messages: []
-      
+      messages: [],
+      rebaseBinding: null,
     }
   }
 
-  componentDidMount(){
-    base.syncState(`${this.props.room.name}/messages`, {
+  componentDidMount() {
+    this.syncMessages()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.room.name !== this.props.room.name) {
+      this.syncMessages()
+    }
+  }
+
+  syncMessages = () => {
+    // Stop syncing with the current endpoint
+    if (this.state.rebaseBinding) {
+      base.removeBinding(this.state.rebaseBinding)
+    }
+
+    // sync with the new endpoint
+    const rebaseBinding = base.syncState(`${this.props.room.name}/messages`, {
       context: this,
       state: 'messages',
       asArray: true,
-
     })
+
+    this.setState({ rebaseBinding })
   }
 
   addMessage = (body) => {
@@ -38,8 +55,11 @@ class Chat extends Component {
   render() {
     return (
       <div className="Chat" style={styles}>
-        <ChatHeader room={this.props.room}/>
-        <MessageList messages={this.state.messages} room={this.props.room}/>
+        <ChatHeader room={this.props.room} />
+        <MessageList
+          messages={this.state.messages}
+          room={this.props.room}
+        />
         <MessageForm addMessage={this.addMessage} />
       </div>
     )
