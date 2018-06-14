8fe2707283ea2530dd7608289afcf18d660feb5f
kyle wood <kylewood777@oru.edu>
2018-06-14 15:24:45 -0400

    show and hide new room form

--- src/RoomLink.js
+++ src/RoomLink.js
@@ -1,50 +1,43 @@
 import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
-const RoomLink = ({room, loadRoom}) => {
-    const handleClick=(ev)=>{
-        ev.preventDefault()
-        loadRoom(room)
-    }
+const RoomLink = ({ room, loadRoom }) => {
+  const handleClick = (ev) => {
+    ev.preventDefault()
+    loadRoom(room)
+  }
+
   return (
     <li className={css(styles.item)}>
-          <a href="/" className={css(styles.link)} onClick={handleClick}>{room.name}</a>
+      <a
+        href="/"
+        className={css(styles.link)}
+        onClick={handleClick}
+      >
+        {room.name}
+      </a>
     </li>
   )
 }
 
 const styles = StyleSheet.create({
-    nav: {
-      padding: '0 1rem',
+  item: {
+    marginBottom: '0.5rem',
+  },
+
+  link: {
+    display: 'block',
+    color: 'whitesmoke',
+    textDecoration: 'none',
+
+    '::before': {
+      content: '"# "',
     },
-  
-    h2: {
-      fontSize: '1rem',
-    },
-  
-    list: {
-      listStyle: 'none',
-      marginLeft: 0,
-      paddingLeft: 0,
-    },
-  
-    item: {
-      marginBottom: '0.5rem',
-    },
-  
-    link: {
-      display: 'block',
-      color: 'whitesmoke',
-      textDecoration: 'none',
-  
-      '::before': {
-        content: '"# "',
-      },
-  
-      ':hover': {
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-      }
-    },
-  })
+
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    }
+  },
+})
 
 export default RoomLink
