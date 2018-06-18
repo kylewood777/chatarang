2da9153954c2014f192791fabd57b52947e15276
kyle wood <kylewood777@oru.edu>
2018-06-18 11:34:30 -0400

    add github login

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
-import { auth, googleProvider } from './base'
+import { auth, googleProvider, gitProvider, facebookProvider} from './base'
 
 class SignIn extends Component {
   state = {
@@ -17,8 +17,16 @@ class SignIn extends Component {
     // do something?
   }
 
-  authenticate = () => {
+  authenticateGoogle = () => {
     auth.signInWithPopup(googleProvider)
+  }
+
+  authenticateGit = () => {
+    auth.signInWithPopup(gitProvider)
+  }
+
+  authenticateFacebook = () => {
+    auth.signInWithPopup(facebookProvider)
   }
 
   render() {
@@ -54,10 +62,26 @@ class SignIn extends Component {
             <button
               type="button"
               className={css(styles.button)}
-              onClick={this.authenticate}
+              onClick={this.authenticateGoogle}
             >
               <i className={`fab fa-google ${css(styles.brandIcon)}`}></i>
               Sign in with Google
+            </button>
+            <button
+              type="button"
+              className={css(styles.button)}
+              onClick={this.authenticateGit}
+            >
+              <i className={`fab fa-github ${css(styles.brandIcon)}`}></i>
+              Sign in with GitHub
+            </button>
+            <button
+              type="button"
+              className={css(styles.button)}
+              onClick={this.authenticateFacebook}
+            >
+              <i className={`fab fa-facebook ${css(styles.brandIcon)}`}></i>
+              Sign in with Facebook
             </button>
           </form>
 
